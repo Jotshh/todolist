0ad23b76d4337f03588a1d617df5568c976087b8
Josiel <josiephelipel265@gmail.com>
2026-02-24 21:29:27 -0400

    add metodos get e set para ler e definir valores

--- todolist/src/main/java/br/com/josielphelipe/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/josielphelipe/todolist/user/UserModel.java
@@ -1,7 +1,33 @@
 package br.com.josielphelipe.todolist.user;
 
 public class UserModel {
-    public String username;
-    public String password;
-    public String name;
+
+    private String username;
+    private String password;
+    private String name;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }   
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
