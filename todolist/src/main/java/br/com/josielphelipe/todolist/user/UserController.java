526d47c633b41bff58dff473d9b759009946274d
Josiel <josiephelipel265@gmail.com>
2026-02-25 17:21:41 -0400

    add validacao de usuario existente no banco

--- todolist/src/main/java/br/com/josielphelipe/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/josielphelipe/todolist/user/UserController.java
@@ -16,6 +16,12 @@ public class UserController {
 
    @PostMapping("/")  
     public UserModel create(@RequestBody UserModel userModel) {
+
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+            if(user != null) {
+                System.out.println("Usuário existente");
+                return null;
+            }
         var userCreated = this.userRepository.save(userModel);
         return userCreated;
     }
