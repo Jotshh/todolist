d948df10a61c1c198fc473ea7af24347571c5f8e
Josiel <josiephelipel265@gmail.com>
2026-02-28 16:38:49 -0400

    add metodo list para ver todas as tarefas do usuario

--- todolist/src/main/java/br/com/josielphelipe/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/josielphelipe/todolist/task/TaskController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -40,6 +42,13 @@ public class TaskController {
 
         var task = this.taskRepository.save(taskModel);  
         return ResponseEntity.status(HttpStatus.OK).body(task);     
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID)idUser);
+        return tasks;
     }
 
 }
