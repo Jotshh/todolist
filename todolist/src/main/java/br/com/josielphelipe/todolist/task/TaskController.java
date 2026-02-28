7ffaa3002bc26df5c09bf4a9804f22a7119858fb
Josiel <josiephelipel265@gmail.com>
2026-02-28 18:43:49 -0400

    add utility method to copy non-null properties and update task model in TaskController

--- todolist/src/main/java/br/com/josielphelipe/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/josielphelipe/todolist/task/TaskController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.josielphelipe.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -54,12 +55,13 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-        
-        var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {  
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+ 
+        return this.taskRepository.save(task);
 
     }
 }
